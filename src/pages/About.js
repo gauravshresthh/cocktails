38a5f8e25471e415fad3d384360f13750d82dd47
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-08-16 23:07:29 +0545

    error and about page setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
+        soluta similique optio ea corporis consectetur atque ad! Earum sit
+        deleniti quos vitae quam enim. In provident id consectetur blanditiis
+        libero natus sequi odit numquam, dignissimos recusandae temporibus eos
+        sint officia iusto voluptatem adipisci illo fugit alias harum? Eius,
+        laborum minus!
+      </p>
+    </section>
+  );
 }
